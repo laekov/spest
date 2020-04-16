2a9a73c0829b12f5b6b0389afb20669339529467
Rick Ho <laekov.h@gmail.com>
2020-04-16 14:49:38 +0800

    batched train

--- assembler/train.py
+++ assembler/train.py
@@ -1,26 +1,32 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 import os
 import random
 
 from model.model import PerfCompare
-from dataloader import DataLoader
+from dataloader import MatrixData, ConfigurationData
 from emave import ExpMovAve
 import config
 
 
 def main():
-    dataset = DataLoader(config.data_path)
+    dataset = ConfigurationData(config.conf_data_path)
+    # dataset = MatrixData(config.matrix_data_path)
     model = PerfCompare()
 
     if config.load_path is not None and os.path.exists(config.load_path):
         model.load_state_dict(torch.load(config.load_path))
 
+    loss_weight = torch.tensor([1., 1., .001])
+
     if config.cuda:
         model.cuda()
+        loss_weight = loss_weight.cuda()
 
     optim = torch.optim.Adam(model.parameters(), lr=config.lr, weight_decay=config.weight_decay)
+    model.train()
 
     loss_accum = ExpMovAve()
     accu_accum = ExpMovAve()
@@ -28,36 +34,40 @@ def main():
 
     for i in range(config.train_iters):
         labels = []
-        outs = []
+        datas = []
+
         for _ in range(config.batch_size):
             data, idxs, label = dataset.next()
             data = data.cuda()
-            out = model.est(data, idxs)
+            datas.append(data)
             labels = labels + label
-            outs.append(out)
 
-        outs = torch.cat(outs, dim=0)
-        
+        datalen = max([d.shape[0] for d in datas])
+        datas = [F.pad(d, (0, 0, 0, datalen - d.shape[0])) for d in datas]
+        datas = torch.stack(datas)
+        outs = model.est(datas)
+
         cmpa, cmpb = [], []
         answer = []
         for _ in range(config.comp_size):
             ia, ib = random.randint(0, len(labels) - 1), random.randint(0, len(labels) - 1)
-            cmpa.append(outs[ia])
-            cmpb.append(outs[ib])
-            ans = 2
-            if labels[ia] < labels[ib]:
+            if abs(labels[ia] - labels[ib]) / max(labels[ia], labels[ib]) < config.cmp_eps:
+                continue
+            elif labels[ia] < labels[ib]:
                 ans = 0
             elif labels[ia] > labels[ib]:
                 ans = 1
             answer.append(ans)
+            cmpa.append(ia)
+            cmpb.append(ib)
 
-        cmpa = torch.stack(cmpa)
-        cmpb = torch.stack(cmpb)
+        cmpa = outs[cmpa]
+        cmpb = outs[cmpb]
         answer = torch.tensor(answer)
         if config.cuda:
             answer = answer.cuda()
         cmp_res = model.comp(cmpa, cmpb)
-        loss = F.cross_entropy(cmp_res, answer)
+        loss = F.cross_entropy(cmp_res, answer, weight=loss_weight)
         if torch.isnan(loss):
             raise ValueError('Loss becomes NaN! Last datasets are: \n{}'.format(
                 dataset.get_last_records(64)))
