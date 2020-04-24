c5b497496c634720af1f97d31bc66bd86229eab4
Rick Ho <laekov.h@gmail.com>
2020-04-24 10:02:26 +0800

    add hash lookup for tracing

--- include/spest/hash.h
+++ include/spest/hash.h
@@ -2,8 +2,11 @@
 #ifndef HASH_H
 #define HASH_H
 
+#include <string>
+
 typedef unsigned long long hash_t;
 
 hash_t hashCallerInfo(const char* file, int line);
+std::string lookupHashRecord(hash_t hash);
 
 #endif
--- src/hash.cc
+++ src/hash.cc
@@ -1,9 +1,25 @@
 #include <spest/hash.h>
 
+#include <sstream>
+#include <unordered_map>
+
+std::unordered_map<hash_t, std::pair<std::string, int> > _hash_record;
+
+std::string lookupHashRecord(hash_t hash) {
+	auto it = _hash_record.find(hash);
+	if (it == _hash_record.end()) {
+		return "Hash not found";
+	}
+	std::ostringstream oss;
+	oss << it->second.first << ":" << it->second.second;
+	return oss.str();
+}
+
 hash_t hashCallerInfo(const char* file, int line) {
 	hash_t res = 0;
 	for (const char* i = file; *i; ++i) {
 		res = res * 37 + *i;
 	}
+	_hash_record[res ^ line] = std::pair<std::string, int>(std::string(file), line);
 	return res ^ line;
 }
