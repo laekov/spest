2b4fc2ce9b998210ea6db6feb19b82d708f97d5c
Rick Ho <laekov.h@gmail.com>
2020-04-26 20:46:52 +0800

    better accuracy?

--- src/tb_sim.cc
+++ src/tb_sim.cc
@@ -24,6 +24,8 @@ cnt_t TBSim::calculate(int num_threads) {
 	cnt_t max_line = 0;
 	cnt_t tot_trans = 0;
 	for (auto& line : global_lds) {
+		// std::cerr << lookupHashRecord(line.first) << std::endl;
+
 		size_t maxlen = 0;
 		for (auto& v : line.second) {
 			maxlen = std::max(maxlen, v.size());
@@ -43,25 +45,32 @@ cnt_t TBSim::calculate(int num_threads) {
 				 * all the same memory visit
 				 * should be merged (?)
 				 */
-				tot_trans += 1;
+				// tot_trans += gpu->getGlobalMemLat(num_threads, 4) * ((addrs.size() + 1) / 8);
+				tot_trans += 1.; // / gpu->getGlobalMemLat(num_threads, 4);
+				// std::cerr << lookupHashRecord(line.first) << " " << addrs[0] << " " << addrs.size() << "\n";
 			} else {
 				/*
 				 * not all the same, should look for merge
 				 */
+				double local_sum = 0;
 				for (size_t i = 0, j; i < addrs.size(); ) {
 					for (j = i + 1; j < addrs.size() && addrs[j] == addrs[i]; ++j);
 					if (j > i + 1) {
+						// std::cerr << "SAME " << addrs[j] << " " << j - i << "\n";
 						i = j;
-						tot_trans += gpu->getGlobalMemLat(num_threads, 4);
+						local_sum += (j - i) / gpu->getGlobalMemLat(num_threads, 4) / 1.5;
 						continue;
 					}
-					for (j = i + 1; j < addrs.size() && addrs[j] == addrs[j - 1] + 4 && j - i < 32; ++j);
-					tot_trans += gpu->getGlobalMemLat(num_threads, (j - i));
+					for (j = i + 1; j < addrs.size() && addrs[j] == addrs[j - 1] + 4; ++j);
+					local_sum += (j - i) / gpu->getGlobalMemLat(num_threads, (j - i));
+					// std::cerr << "CONT " << addrs[j] << " " << j - i << "\n";
 					i = j;
 				}
+				tot_trans += 1.; // local_sum / addrs.size();
 			}
 		}
 	}
-	// std::cout << tot_access << " " << max_line << " " << tot_trans << "\n";
+	// exit(0);
+	// std::cout << tot_trans << "\n";
 	return tot_trans;
 }
